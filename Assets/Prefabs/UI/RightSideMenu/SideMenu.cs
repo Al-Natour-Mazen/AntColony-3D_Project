ec449fc7a9c83e3f2f9b0428d5c60910dd880ab2
Léo Hervouet <leoohervouet@gmail.com>
2024-04-19 18:07:28 +0200

    Add the possibility to change the menu width
    In the RightSideMenu prefab

--- Assets/Prefabs/UI/RightSideMenu/SideMenu.cs
+++ Assets/Prefabs/UI/RightSideMenu/SideMenu.cs
@@ -2,21 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 
 public class SideMenu : MonoBehaviour
 {
+    [Header("Menu Settings")]
+    public float widthMenu = 400.0f;
+
     [Header("Button Settings")]
     public Sprite buttonSprite;
 
-
     void OnValidate()
     {
+        // To change the sprite of the button.
         Button button = gameObject.GetComponentInChildren<Button>();
         if(button != null && buttonSprite != null) 
         { 
             Image buttonImage = button.image;
             buttonImage.sprite = buttonSprite;
-            Debug.Log("spriteChanged");
         }
+
+        // To change the width of the menu.
+        RectTransform rectT = gameObject.GetComponent<RectTransform>();
+        EditorApplication.delayCall += () =>
+        {
+            if(rectT != null )
+            {
+                Vector2 currentSize = rectT.sizeDelta;
+                currentSize.x = widthMenu;
+                rectT.sizeDelta = currentSize;
+            }
+        };
+        
     }
 }
