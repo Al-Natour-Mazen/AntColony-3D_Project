1af1172872100e52bdff1a2647ee1ecaaedad636
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-04-21 01:44:56 +0200

    Infos Tab Image Button
    add Image to this button in that menu and adjuste the color if the button is disabled

--- Assets/Prefabs/UI/Menus/Scripts/Menu.cs
+++ Assets/Prefabs/UI/Menus/Scripts/Menu.cs
@@ -34,6 +34,19 @@ public class Menu : MonoBehaviour
         if(button != null)
         {
             button.enabled = state;
+            // Get the Image component of the button
+            Image image = button.GetComponent<Image>();
+
+            if (!button.enabled)
+            {
+                // Change the color of the image to #5C5050 with alpha 112 when the button is not enabled
+                image.color = new Color32(92, 80, 80, 112);
+            }
+            else
+            {
+                // Change the color of the image back to normal when the button is enabled
+                image.color = Color.white;
+            }
         }
     }
 }
