31d1ffbddff89d8d83eae2b7bc1bd97ff1443c6f
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-04-21 23:28:44 +0200

    Update SimulationController.cs
    Factorize the controller to better understanding

--- Assets/Scripts/Controller/SimulationController.cs
+++ Assets/Scripts/Controller/SimulationController.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 public class SimulationController : MonoBehaviour
 {
-
     [Header("Board Settings")]
     public Board board;
     public int width = 20;
-    public int height = 10;
+    public int height = 20;
 
     [Header("Entity Counts")]
-    public int antCount = 10;
-    public int wallCountDensity = 10;
-    public int seedCountDensity = 10;
-
-    [Header("Seed Settings")]
-    public int maxSeedQuantityPerBlock = 4;
-
-   
+    public int antCount = 15;
+    public int wallCountDensity = 80;
+    public int seedCountDensity = 57;
+    public int maxSeedQuantityPerBlock = 10;
 
     [Header("UI Settings")]
     public ProgressBar progressBar;
@@ -41,81 +36,71 @@ public class SimulationController : MonoBehaviour
     private float simulationSpeed = 1.0f;
     private float elapsedTime = 0f;
 
-
     void Start()
     {
-        InitSimulation();
+        InitializeSimulation();
     }
 
-    private void InitSimulation()
+    private void InitializeSimulation()
     {
-        // Initialisation du modèle avec les données
         antSimulation = new AntConlonySimulation(width, height, maxSeedQuantityPerBlock, wallCountDensity, antCount, seedCountDensity);
+        InitializeBoard();
+        StartCoroutine(InitializeUIComponents());
+    }
 
-        // Initialisation de la vue avec les données du modèle
+    private void InitializeBoard()
+    {
         board.SetHeightBoard(height);
         board.SetWidthBoard(width);
         board.InstantiateAnthill(antSimulation);
         board.GenerateWalls(antSimulation);
         board.GenerateAnts(antSimulation);
         board.GenerateSeeds(antSimulation);
-
-        // Start the coroutine to initialize the progress bar
-        StartCoroutine(InitializeUIComponents());
     }
 
-    // Coroutine to initialize the progress bar
     private IEnumerator InitializeUIComponents()
     {
         // Wait for the end of the frame. This ensures that all other Start and Update methods have been called for this frame.
         // This is useful when we want to make sure that all other objects and their scripts have been initialized before using them.
         yield return new WaitForEndOfFrame();
-
+       
         // Now that we are sure everything is initialized, we can set the max value of the progress bar.
         // We use the total number of seeds outside the colony as the max value.
         progressBar.SetMaxValue(antSimulation.GetTotalSeedOutColony());
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
         simulationSpeed = sliderTextView.GetCurrentValue();
         dynamicTextViewTime.DynamicText = FormatTime(elapsedTime);
         dynamicTextViewAnt.DynamicText = antSimulation.GetAntsInColony().Count.ToString();
         dynamicTextViewSeedInColony.DynamicText = antSimulation.GetTotalSeedInColony().ToString();
         dynamicTextViewSeedOutColony.DynamicText = antSimulation.GetTotalSeedOutColony().ToString();
+
+        inputfieldAnt.SetInputFieldValue(antCount.ToString());
+        inputfieldWallDensity.SetInputFieldValue(wallCountDensity.ToString());
+        inputfieldSeedDensity.SetInputFieldValue(seedCountDensity.ToString());
+        inputfieldSeedPerBlock.SetInputFieldValue(maxSeedQuantityPerBlock.ToString());
+        inputfieldWidthSimulation.SetInputFieldValue(width.ToString());
+        inputfieldHeightSimulation.SetInputFieldValue(height.ToString());
     }
+
     void Update()
     {
         float currentTime = Time.fixedTime;
         float sliderValue = sliderTextView.GetCurrentValue();
 
-        if (sliderValue <= 0)
-        {
-            simulationSpeed = 1.0f;
-        }
-        else
-        {
-            // Inverse the slider value before assigning it to simulationSpeed
-            simulationSpeed = (1.2f / (sliderValue + 0.001f * 100));
-        }
+        UpdateSimulationSpeed(sliderValue, currentTime);
 
         if (playPauseButton.IsPlaying())
         {
             if (simulationSpeed <= (currentTime - lastTime))
             {
-                menu.DisableButton();
-
-                // Mise à jour du modèle
-                antSimulation.EvolveTheAntColony();
-
-                dynamicTextViewSeedInColony.DynamicText = antSimulation.GetTotalSeedInColony().ToString();
-                dynamicTextViewSeedOutColony.DynamicText = antSimulation.GetTotalSeedOutColony().ToString();
-
-                // Mise à jour de la vue avec les données du modèle
-                progressBar.AdvanceProgress(antSimulation.GetTotalSeedInColony());
-                board.GenerateAnts(antSimulation);
-                board.GenerateSeeds(antSimulation);
-
-                lastTime = currentTime; // Met à jour le temps de la dernière mise à jour
+                UpdateSimulation(currentTime);
             }
 
-            elapsedTime += Time.deltaTime * sliderValue; // Always increase elapsedTime when the simulation is playing
+            elapsedTime += Time.deltaTime * sliderValue;
             dynamicTextViewTime.DynamicText = FormatTime(elapsedTime);
         }
         else
@@ -124,7 +109,29 @@ public class SimulationController : MonoBehaviour
         }
     }
 
-    // Méthode pour formater le temps en format lisible (heures:minutes:secondes)
+    private void UpdateSimulationSpeed(float sliderValue, float currentTime)
+    {
+        if (sliderValue <= 0)
+        {
+            simulationSpeed = 1.0f;
+        }
+        else
+        {
+            simulationSpeed = (1.2f / (sliderValue + 0.001f * 100));
+        }
+    }
+
+    private void UpdateSimulation(float currentTime)
+    {
+        menu.DisableButton();
+        antSimulation.EvolveTheAntColony();
+        UpdateUI();
+        progressBar.AdvanceProgress(antSimulation.GetTotalSeedInColony());
+        board.GenerateAnts(antSimulation);
+        board.GenerateSeeds(antSimulation);
+        lastTime = currentTime;
+    }
+
     private string FormatTime(float timeInSeconds)
     {
         int hours = Mathf.FloorToInt(timeInSeconds / 3600f);
@@ -135,20 +142,16 @@ public class SimulationController : MonoBehaviour
 
     public void InitOtherEnvironement()
     {
-        int numberOfAnt, wallDensity, seedDensity, maxSeedBlock;
-
-        bool isParseSuccessfulAnt = int.TryParse(inputfieldAnt.GetInputFieldValue(), out numberOfAnt);
-        bool isParseSuccessfulWall = int.TryParse(inputfieldWallDensity.GetInputFieldValue(), out wallDensity);
-        bool isParseSuccessfulSeed = int.TryParse(inputfieldSeedDensity.GetInputFieldValue(), out seedDensity);
-        bool isParseSuccessfulSeedBlock = int.TryParse(inputfieldSeedPerBlock.GetInputFieldValue(), out maxSeedBlock);
-     
-        if (isParseSuccessfulAnt && isParseSuccessfulWall && isParseSuccessfulSeed && isParseSuccessfulSeedBlock)
+        if (TryParseInt(inputfieldAnt.GetInputFieldValue(), out int numberOfAnt) &&
+            TryParseInt(inputfieldWallDensity.GetInputFieldValue(), out int wallDensity) &&
+            TryParseInt(inputfieldSeedDensity.GetInputFieldValue(), out int seedDensity) &&
+            TryParseInt(inputfieldSeedPerBlock.GetInputFieldValue(), out int maxSeedBlock))
         {
             antCount = numberOfAnt;
             wallCountDensity = wallDensity;
             seedCountDensity = seedDensity;
             maxSeedQuantityPerBlock = maxSeedBlock;
-            this.InitSimulation();
+            InitializeSimulation();
         }
         else
         {
@@ -158,21 +161,21 @@ public class SimulationController : MonoBehaviour
 
     public void InitOtherDimension()
     {
-        int width, height ;
-
-        bool isParseSuccessfulWidth = int.TryParse(inputfieldWidthSimulation.GetInputFieldValue(), out width);
-        bool isParseSuccessfulHeight = int.TryParse(inputfieldHeightSimulation.GetInputFieldValue(), out height);
-    
-        if (isParseSuccessfulWidth && isParseSuccessfulHeight)
+        if (TryParseInt(inputfieldWidthSimulation.GetInputFieldValue(), out int width) &&
+            TryParseInt(inputfieldHeightSimulation.GetInputFieldValue(), out int height))
         {
             this.width = width;
             this.height = height;
-
-            this.InitSimulation();
+            InitializeSimulation();
         }
         else
         {
             Debug.Log("Error in InitOtherEnvironement Function");
         }
+    }
+
+    private bool TryParseInt(string input, out int result)
+    {
+        return int.TryParse(input, out result);
     }
 }
