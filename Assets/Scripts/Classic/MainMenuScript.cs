d0c17851b35ceace3e57e69eb1d92df8d7d98be5
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-05-07 18:51:00 +0200

    Buttons MainsScene
    add all actions to all mainscene buttons

--- Assets/Scripts/Classic/MainMenuScript.cs
+++ Assets/Scripts/Classic/MainMenuScript.cs
@@ -1,24 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void StartSimulation()
     {
         SceneManager.LoadSceneAsync("Simulation");
+    }
+
+    public void QuitSimulationApp()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    public void OpenUrl(string url)
+    {
+        Application.OpenURL(url);
     }
 }
